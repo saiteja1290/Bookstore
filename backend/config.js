a2bf90566831a5685a1ea504ecb6382232375152
saiteja.palegar@gmail.com <saiteja.palegar@gmail.com>
2024-03-09 11:44:13 +0530

    asdas

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,5 @@
+import dotenv from "dotenv";
+
+dotenv.config()
 export const PORT = 5555;
-export const mongourl = "mongodb+srv://saiteja:saiteja1290@cluster0.jvtt3so.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+export const mongourl = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.jvtt3so.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
